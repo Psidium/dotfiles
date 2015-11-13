752a61a44b9dc7006c1ed72c68e0455c06f609d8
Psidium <psiidium@gmail.com>
2015-11-13 00:43:23 -0200

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,14 @@
-function fish_prompt
-        /Users/Psidium/Documents/random/powerline-shell/powerline-shell.py $status --shell bare ^~/Documents/output-powerline-shell.log
-end
+#function fish_prompt
+#        /Users/Psidium/Documents/random/powerline-shell/powerline-shell.py $status --shell bare ^~/Documents/output-powerline-shell.log
+#end
+#
+#
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+powerline-daemon -q
+
+set fish_function_path $fish_function_path "/usr/local/lib/python2.7/site-packages/powerline/bindings/fish"
+
+powerline-setup
+
+eval (docker-machine env default)
