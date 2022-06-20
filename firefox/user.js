5633430e294984bd3d6087d7e746ec59387e8974
Gabriel Borges <psiidium@gmail.com>
2022-06-20 10:58:44 -0300

    save ff

---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("security.enterprise_roots.enabled", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("security.osclientcerts.autoload", true);
+user_pref("services.sync.addons.trustedSourceHostnames", "addons.mozilla.org,nikolockenvitz.github.io,1password.com");
