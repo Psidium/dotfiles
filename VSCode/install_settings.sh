c13fe2584c3f75e72c89c0130f408adf14edc73c
Gabriel Borges <gabriel.borges@sap.com>
2022-07-14 19:58:54 -0300

    add vscode

---
+++ VSCode/install_settings.sh
@@ -0,0 +1,4 @@
+ln -s $(realpath ./settings.json) ~/Library/Application\ Support/Code/User/settings.json
+ln -s $(realpath ./keybindings.json) ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s $(realpath ./snippets/) ~/Library/Application\ Support/Code/User
+
