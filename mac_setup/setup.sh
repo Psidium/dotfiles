1bac7a1a9f6850e780a4ce8735e3f0ac140b460a
Gabriel Borges <gabriel.borges@sap.com>
2022-07-15 23:00:53 -0300

    update

--- mac_setup/setup.sh
+++ mac_setup/setup.sh
@@ -7,6 +7,7 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 xcode-select --install || echo 'ja tem xcode tools'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install \
     git \
@@ -51,7 +52,7 @@ asdf global nodejs 14.18.2
 asdf global python latest
 
 asdf plugin add java
-asdf install java openjdk-11
+asdf install java openjdk-18
 asdf install java liberica-jre-javafx-17.0.2+9
 asdf global java liberica-jre-javafx-17.0.2+9
 
@@ -59,9 +60,10 @@ asdf plugin add golang
 asdf install golang latest
 asdf global golang latest
 
-asdf plugin add ruby
-asdf install ruby 3.1.2
-asdf global ruby 3.1.2
+# installing ruby on m1 is crazyyy commented out now
+# asdf plugin add ruby
+# asdf install ruby 3.1.2
+# asdf global ruby 3.1.2
 
 asdf plugin add kubectl
 asdf install kubectl latest
@@ -78,10 +80,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 nvim -c PlugInstall
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-rm ~/.zshrc
-ln -s $(realpath ../.zshrc) ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm ~/.zshrc
+ln -s $(realpath ../.zshrc) ~/.zshrc
 
 wait
 for extension in "activitywatch.aw-watcher-vscode" "apollographql.vscode-apollo" "arcanis.vscode-zipfs" "asvetliakov.snapshot-tools" "bierner.markdown-mermaid" "dbaeumer.vscode-eslint" "eamodio.gitlens" "EditorConfig.EditorConfig" "esbenp.prettier-vscode" "flowtype.flow-for-vscode" "gamunu.vscode-yarn" "jasonnutter.vscode-codeowners" "letmaik.git-tree-compare" "ms-python.python" "ms-python.vscode-pylance" "ms-toolsai.jupyter" "ms-toolsai.jupyter-keymap" "ms-toolsai.jupyter-renderers" "redhat.java" "redhat.vscode-xml" "redhat.vscode-yaml" "streetsidesoftware.code-spell-checker" "tamasfe.even-better-toml" "teamchilla.blueprint" "tomoyukim.vscode-mermaid-editor" "VisualStudioExptTeam.vscodeintellicode" "vscjava.vscode-java-debug" "vscjava.vscode-java-dependency" "vscjava.vscode-java-pack" "vscjava.vscode-java-test" "vscjava.vscode-maven" "vscodevim.vim" "yzhang.markdown-all-in-one"
